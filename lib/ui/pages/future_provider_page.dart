894d76f6e9e6e6e375b9d51bd9732bbd72109b94
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2023-02-01 22:05:35 -0300

    future_provider_page.dart

--- lib/ui/pages/future_provider_page.dart
+++ lib/ui/pages/future_provider_page.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod2_temi/ui/services/api_service.dart';
 
-class FutureProviderPage extends StatelessWidget {
+import '../models/suggestion.dart';
+
+final suggestionFutureProvider =
+    FutureProvider.autoDispose<Suggestion>((ref) async {
+  final apiService = ref.watch(apiServiceProvider);
+  return apiService.getSuggestion('5');
+});
+
+class FutureProviderPage extends ConsumerWidget {
   const FutureProviderPage({Key? key, required this.color}) : super(key: key);
 
   final Color color;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<Suggestion> suggestionRef =
+        ref.watch(suggestionFutureProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
         backgroundColor: color,
       ),
-      body: const Center(child: Text('Future Provider')),
+      body: RefreshIndicator(
+        onRefresh: () => ref.refresh(suggestionFutureProvider.future),
+        child: ListView(children: [
+          suggestionRef.when(
+            data: (data) => Text(data.activity),
+            error: (error, stackTrace) =>
+                Text('$error.toString()\n $stackTrace.toString()'),
+            loading: () => const CircularProgressIndicator(),
+          ),
+        ]),
+      ),
     );
   }
 }
